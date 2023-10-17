f91c6582a0191d6db622ad047e0a20fa6895f285
JoshuaDeBord <joshua.debord@gctcok.edu>
2023-10-17 12:47:25 -0500

    f

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.XR;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    public Vector3 offset = new(10.7399998f, 0.0500000007f, 1.55999994f);
+    
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+    public float turnSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame 
+    void FixedUpdate()
+    {
+        //assgins the forward input from the keyboard to forwardInput
+        forwardInput = Input.GetAxis("Vertical");
+
+        //makes the plane rotate off the the Z coord smoothly and at rotationSpeed speed and only executes if forward input is given
+        transform.Rotate(Vector3.right * Time.deltaTime * rotationSpeed *  forwardInput);
+
+        //makes the plane fly at a constant speed smoothly
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+    }
+}
